ce72b456cf32b18f162a1fb3cff70fb6f711f28d
Ish Kumar <ish1301@gmail.com>
2023-12-19 15:46:06 -0500

    Bootstrap image analysis module

---
+++ iapp/iapp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for iapp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iapp.settings')
+
+application = get_asgi_application()
