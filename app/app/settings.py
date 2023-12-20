b87d733f747d8e48bb3691576b6add1d8fcafb71
Ish Kumar <ish1301@gmail.com>
2023-12-20 09:53:22 -0500

    Change URI to binary for file upload

--- app/app/settings.py
+++ app/app/settings.py
@@ -88,6 +88,7 @@ SPECTACULAR_SETTINGS = {
     "DESCRIPTION": "API project description",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
+    "COMPONENT_SPLIT_REQUEST": True,
 }
 
 # Database
