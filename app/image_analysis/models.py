3e9a21abc8a8f37bc6a5093ce1d4b0a3f63398ca
Ish Kumar <ish1301@gmail.com>
2023-12-19 19:40:24 -0500

    Add model with migrations

--- app/image_analysis/models.py
+++ app/image_analysis/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 MAX_IMAGE_SIZE = 5 * 1024 * 1024  # 5MB
+
+
+class ImageUpload(models.Model):
+    # Avoid storing image
+    filename = models.CharField(max_length=255, blank=True)
+    md5_hash = models.CharField(max_length=32, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
