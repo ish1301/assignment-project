492d067f6ee416083f9e21895c593a128941ce11
Ish Kumar <ish1301@gmail.com>
2023-12-20 09:03:37 -0500

    Update schema for request/response

--- app/image_analysis/views.py
+++ app/image_analysis/views.py
@@ -1,6 +1,7 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
@@ -9,14 +10,13 @@ from .serializers import ImageUploadSerializer
 
 class ImageUploadView(APIView):
     parser_classes = [
-        FormParser,
         MultiPartParser,
     ]
 
     @extend_schema(
         operation_id="POST image for analysis",
         request=ImageUploadSerializer,
-        responses=ImageUploadSerializer,
+        responses={200: ImageUploadSerializer, 400: ValidationError},
     )
     def post(self, request):
         serializer = ImageUploadSerializer(data=request.data)
